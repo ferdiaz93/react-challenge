a3fc631c6d3459e17e95c5158d4b07041a163966
ferdiaz93 <fernandodiaz1209@gmail.com>
2020-09-11 03:35:34 -0300

    Updates

---
+++ precache-manifest.02395c30dcfc1384f46230e972e2f747.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09f4c7b9e7c3777806b30965302a2c5c",
+    "url": "/react-challenge/index.html"
+  },
+  {
+    "revision": "7dd83346c51de6002583",
+    "url": "/react-challenge/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "428175857dfd85f6e666",
+    "url": "/react-challenge/static/css/main.6cc9a4ab.chunk.css"
+  },
+  {
+    "revision": "7dd83346c51de6002583",
+    "url": "/react-challenge/static/js/2.129757a7.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/react-challenge/static/js/2.129757a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "428175857dfd85f6e666",
+    "url": "/react-challenge/static/js/main.bdca8fe3.chunk.js"
+  },
+  {
+    "revision": "6ca65bfe1ca5b5420154",
+    "url": "/react-challenge/static/js/runtime-main.35842b8e.js"
+  }
+]);
